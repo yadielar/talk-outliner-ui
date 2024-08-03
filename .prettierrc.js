03e4ae7c9ae26f9a9df553fdade47c4a39d171b8
Yadiel Arroyo <yadielar@gmail.com>
2024-08-03 13:26:41 -0400

    add prettier and format code

---
+++ .prettierrc.js
@@ -0,0 +1,39 @@
+/** @type {import("prettier").Options} */
+export const config = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'preserve',
+  requirePragma: false,
+  semi: true,
+  singleAttributePerLine: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  overrides: [
+    {
+      files: ['**/*.mdx'],
+      options: {
+        // This stinks, if you don't do this, then an inline component on the
+        // end of the line will end up wrapping, then the next save prettier
+        // will add an extra line break. Super annoying and probably a bug in
+        // prettier, but until it's fixed, this is the best we can do.
+        proseWrap: 'preserve',
+        htmlWhitespaceSensitivity: 'ignore',
+      },
+    },
+  ],
+  // plugins: ['prettier-plugin-tailwindcss'],
+  // tailwindAttributes: ['class', 'className', 'ngClass', '.*[cC]lassName'],
+  // tailwindFunctions: ['clsx', 'cn'],
+};
+
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,39 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'talk-outliner-ui',
-      short_name: 'talk-outliner-ui',
-      description: 'Create dynamic talk outlines.',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: 'talk-outliner-ui',
+        short_name: 'talk-outliner-ui',
+        description: 'Create dynamic talk outlines.',
+        theme_color: '#ffffff',
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+  ],
+});
