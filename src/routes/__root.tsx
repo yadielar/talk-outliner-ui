17a488ae709fa760b7bc050b4c7ce0a88e926c84
Yadiel Arroyo <yadielar@gmail.com>
2024-08-04 01:02:21 -0400

    setup shadcn/ui

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,22 +1,29 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
-import { PWABadge } from '../components/pwa-badge/PWABadge';
+import { ThemeProvider } from '@/components/theme-provider';
+import { ColorSchemeToggle } from '@/components/color-scheme-toggle';
+import { PWABadge } from '@/components/pwa-badge';
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="flex p-3">
+        <div className="flex-1 flex items-center gap-4">
+          <Link to="/" className="[&.active]:font-bold">
+            Home
+          </Link>
+          <Link to="/about" className="[&.active]:font-bold">
+            About
+          </Link>
+        </div>
+        <div className="flex-none">
+          <ColorSchemeToggle />
+        </div>
       </div>
       <hr />
       <Outlet />
       <PWABadge />
       <TanStackRouterDevtools />
-    </>
+    </ThemeProvider>
   ),
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -38,4 +39,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
